109e3a0b8d6fe2dfc10c4ee1b8ea08d668040193
mamunanaqvi96@gmail.com# <mamunanaqvi96@gmail.com>
2019-05-05 04:42:40 +0500

    another Commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          fallback:"style-loader",
+          use: ['css-loader', 'sass-loader']
+        })
+      }]
+    },
+    plugins:[
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/'
+    }
+  };
+};
